d8b6429076e7178aca564b8b2919dda23729f2b9
Chunhui Liu <leeychee@gmail.com>
2017-04-24 11:54:14 +0800

    增加显示数字

--- main.go
+++ main.go
@@ -20,28 +20,39 @@ type line struct{ p1, p2 point }
 var segment int
 var lfile string
 
-var r, r50 float64
+var r, r25, r50 float64
+var fontsize float64
+var fontpath string
 var imgfn string
 
 // contain points on the circle
-var ps []point
+var ps, ts []point
 
 func init() {
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, "用来生成包含等分连线的原先图片。")
 		flag.PrintDefaults()
 	}
-	flag.IntVar(&segment, "s", 12, "将圆按指定数字等分")
+	flag.IntVar(&segment, "s", 10, "将圆按指定数字等分")
 	flag.StringVar(&lfile, "f", "", "定义连线的文件路径，当此参数为空时，将从命令行参数读取")
 	flag.Float64Var(&r, "r", 500, "圆半径，间接指定了图片大小")
+	flag.Float64Var(&fontsize, "z", 20, "字体大小")
+	flag.StringVar(&fontpath, "p", "./luxisr.ttf", "字体路径")
 	flag.StringVar(&imgfn, "o", "output", "输出图片路径")
 	flag.Parse()
+	r25 = r + fontsize
 	r50 = r + 50
 	ps = make([]point, segment+1, segment+1)
-	for i := 1; i < len(ps); i++ {
+	for i := 0; i < len(ps)-1; i++ {
 		x := r50 + r*math.Sin(2*math.Pi/float64(segment)*float64(i))
 		y := r50 - r*math.Cos(2*math.Pi/float64(segment)*float64(i))
-		ps[i] = point{x, y}
+		ps[i+1] = point{x, y}
+	}
+	ts = make([]point, segment+1, segment+1)
+	for i := 0; i < len(ts)-1; i++ {
+		x := r50 + r25*math.Sin(2*math.Pi/float64(segment)*float64(i))
+		y := r50 - r25*math.Cos(2*math.Pi/float64(segment)*float64(i))
+		ts[i+1] = point{x, y}
 	}
 }
 
@@ -83,6 +94,9 @@ func parse(linestr []string) []line {
 
 func draw(fn string, lines []line) {
 	dc := gg.NewContext(int(2*r50), int(2*r50))
+	if _, err := os.Open(fontpath); err == nil {
+		dc.LoadFontFace(fontpath, fontsize)
+	}
 	// Draw white backgroud
 	dc.DrawRectangle(0, 0, 2*r50, 2*r50)
 	dc.SetRGBA(1, 1, 1, 1)
@@ -90,6 +104,10 @@ func draw(fn string, lines []line) {
 	// Draw black circle and lines
 	dc.SetRGBA(0, 0, 0, 1)
 	dc.DrawCircle(r50, r50, r)
+	// Draw point text
+	for i := 1; i < len(ts); i++ {
+		dc.DrawStringAnchored(strconv.Itoa(i), ts[i].x, ts[i].y, 0.5, 0.5)
+	}
 	for _, l := range lines {
 		dc.DrawLine(l.p1.x, l.p1.y, l.p2.x, l.p2.y)
 		//dc.SetLineWidth(5)
